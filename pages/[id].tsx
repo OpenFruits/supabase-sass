a05a39a3531aadfb48e1421b220590890017e420
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-01-20 15:50:03 +0900

    ✨ fetch lesson

---
+++ pages/[id].tsx
@@ -0,0 +1,51 @@
+import type { CustomNextPage, GetStaticPaths, GetStaticProps } from "next";
+import { FluidLayout } from "src/layout/FluidLayout";
+import { supabase } from "src/utils/supabase";
+
+type Lesson = {
+  id: number;
+  title: string;
+  description: string;
+  created_at: any;
+};
+
+type Props = { lesson: Lesson };
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data: lessons } = await supabase.from("lesson").select("id");
+  const paths = lessons?.map(({ id }) => {
+    return {
+      params: {
+        id: id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params: { id } }) => {
+  const { data: lesson } = await supabase.from("lesson").select("*").eq("id", id).single();
+
+  return {
+    props: {
+      lesson,
+    },
+  };
+};
+
+const LessonDetails: CustomNextPage<Props> = (props) => {
+  return (
+    <div className="py-16 px-8 mx-auto w-full max-w-3xl">
+      <h1 className="mb-6 text-3xl">{props.lesson.title}</h1>
+      <p>{props.lesson.description}</p>
+    </div>
+  );
+};
+
+LessonDetails.getLayout = FluidLayout;
+
+export default LessonDetails;
